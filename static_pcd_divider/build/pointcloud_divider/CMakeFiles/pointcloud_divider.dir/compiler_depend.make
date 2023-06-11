08c00a0377d45cdd9ec3ff8a242d4cf5701480f5
sayon <sayondeep0710@rediffmail.com>
2023-06-11 11:02:40 +0530

    added static point cloud divider

---
+++ static_pcd_divider/build/pointcloud_divider/CMakeFiles/pointcloud_divider.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for pointcloud_divider.
+# This may be replaced when dependencies are built.
---
+++ static_pcd_divider/build/pointcloud_divider/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sayon/hoge_ws/src/pointcloud_divider
+# Build directory: /home/sayon/hoge_ws/src/pointcloud_divider/build/pointcloud_divider
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
